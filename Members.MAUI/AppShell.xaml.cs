fe7635da21b6429808bc9c6c7980ab56904a4a6a
QIANG DING <joshua.qj@hotmail.com>
2023-10-30 16:04:12 +1000

    update sqlite library

--- Members.MAUI/AppShell.xaml.cs
+++ Members.MAUI/AppShell.xaml.cs
@@ -1,7 +1,16 @@
-﻿namespace Members.MAUI {
+﻿using Members.MAUI.Views;
+namespace Members.MAUI {
     public partial class AppShell : Shell {
         public AppShell() {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(AddTeamPage), typeof(AddTeamPage));
+            Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+            Routing.RegisterRoute(nameof(AddStudentPage), typeof(AddStudentPage));
+            Routing.RegisterRoute(nameof(EditStudentPage), typeof(EditStudentPage));
+            Routing.RegisterRoute(nameof(EditTeamPage), typeof(EditTeamPage));
+            Routing.RegisterRoute(nameof(StudentsPage), typeof(StudentsPage));
+            Routing.RegisterRoute(nameof(TeamsPage), typeof(TeamsPage));
+            // Routing.RegisterRoute(nameof(EditTeamPage),typeof(AddTeamPage));
         }
     }
 }
