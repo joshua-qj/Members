5acfd0e5f1cf45b442df789aaaf37525f9b9a43a
QIANG DING <joshua.qj@hotmail.com>
2023-11-06 15:58:40 +1000

    Change record view by select by date

--- Members.MAUI/ViewModels/AttendanceRecordsViewModel.cs
+++ Members.MAUI/ViewModels/AttendanceRecordsViewModel.cs
@@ -9,9 +9,16 @@ namespace Members.MAUI.ViewModels {
         private string filterText;
         private readonly IViewAttendanceRecordsUseCase _viewAttendanceRecordsUseCase;
         public ObservableCollection<AttendanceRecord> AttendanceRecords { get; set; }
+        DateTime _time1;
+
+        public DateTime Time1 {
+            get => _time1;
+            set => SetProperty(ref _time1, value);
+        }
         public AttendanceRecordsViewModel(IViewAttendanceRecordsUseCase viewAttendanceRecordsUseCase) {
             _viewAttendanceRecordsUseCase = viewAttendanceRecordsUseCase;
             AttendanceRecords = new ObservableCollection<AttendanceRecord>();
+            Time1 = DateTime.Now;
         }
 
 
@@ -37,11 +44,26 @@ namespace Members.MAUI.ViewModels {
                 }
             }
         }
-
+        private async Task LoadAttendanceRecordsAsyncByDate(DateTime dateTime) {
+            AttendanceRecords.Clear();
+            var attendanceRecords = await _viewAttendanceRecordsUseCase.ExecuteAsync();
+            attendanceRecords = attendanceRecords.Where(a=>a.Date.Day==dateTime.Date.Day).OrderBy(a => a.Student.FirstName).ToList();
+            if (attendanceRecords != null && attendanceRecords.Count > 0) {
+                foreach (var attendance in attendanceRecords) {
+                    this.AttendanceRecords.Add(attendance);
+                }
+            }
+        }
 
         [RelayCommand]
         public async Task SearchBar_TextChangedAsync(string filter) {
             await LoadAttendanceRecordsAsync();
         }
+        [RelayCommand]
+        public async Task LoadAttendanceRecordsByDate() {
+            await LoadAttendanceRecordsAsyncByDate(_time1);
+        }
+
+
     }
 }
