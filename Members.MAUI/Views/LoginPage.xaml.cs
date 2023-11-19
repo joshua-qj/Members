a2135ec0e45a0e418a302e8979226b3c085aa2b0
joshu <joshua.qj@hotmail.com>
2023-11-19 11:10:38 +1000

    Add Login page

---
+++ Members.MAUI/Views/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+namespace Members.MAUI.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+    }
+    protected override bool OnBackButtonPressed() {
+        Application.Current.Quit();
+        return true;
+    }
+
+    private async void LoginButton_Clicked(object sender, EventArgs e) {
+        if (IsCredentialCorrect(Username.Text, Password.Text)) {
+            await SecureStorage.SetAsync("hasAuth", "true");
+            await Shell.Current.GoToAsync("///AttendanceRecordsPage");
+        } else {
+            await DisplayAlert("Login failed", "Uusername or password if invalid", "Try again");
+        }
+    }
+
+
+    bool IsCredentialCorrect(string username, string password) {
+        return Username.Text == "admin" && Password.Text == "1234";
+    }
+}
